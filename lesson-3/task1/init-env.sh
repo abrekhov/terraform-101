8b5ab80aa4423f92034410734610d043b3d1c5df
Anton Brekhov <tony.brekhov@gmail.com>
2023-09-10 18:47:46 +0300

    fx delete sensitive info

--- lesson-3/task1/init-env.sh
+++ lesson-3/task1/init-env.sh
@@ -2,4 +2,3 @@
 export YC_FOLDER_ID=`yc config get folder-id`
 export YC_CLOUD_ID=`yc config get cloud-id`
 export YC_SERVICE_ACCOUNT_KEY_FILE="./key.json"
-export AWS_ACCESS_KEY_ID=YCF08Q7XugH8CTPiti_aD2yRg; export AWS_SECRET_ACCESS_KEY=YCP0G3w2kyDw_XegW14iNLb6_b2KPpNrwDWikWEw
